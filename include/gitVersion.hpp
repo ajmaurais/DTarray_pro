06fc48886825dc1b3f87b73251d1f3709c8d551e
mauraisa <aaron.maurais@bc.edu>
2018-03-13 12:52:29 -0400

    Making changes to man file.

--- include/gitVersion.hpp
+++ include/gitVersion.hpp
@@ -2,7 +2,7 @@
 #ifndef gitVersion_hpp
 #define gitVersion_hpp
 
-const char* GIT_DATE = "Mon Feb 26 18:26:55 2018";
-const char* GIT_COMMIT = "d9a764a8f14214cf0f6a0bceca8ae204d0432dca";
+const char* GIT_DATE = "Mon Feb 26 18:27:49 2018";
+const char* GIT_COMMIT = "9d67b1ab5eca9768d5648cbefcc294ae86df5d8c";
 
 #endif /* gitVersion_hpp */
