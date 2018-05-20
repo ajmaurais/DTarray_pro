4584fbfb6678a23f8b4bda704f88f1714582ab76
mauraisa <aaron.maurais@bc.edu>
2018-05-20 15:55:27 -0400

    Adding header to db/defaultResidueAtoms.txt

--- include/gitVersion.hpp
+++ include/gitVersion.hpp
@@ -2,7 +2,7 @@
 #ifndef gitVersion_hpp
 #define gitVersion_hpp
 
-const char* GIT_DATE = "Tue Mar 13 12:52:29 2018";
-const char* GIT_COMMIT = "06fc48886825dc1b3f87b73251d1f3709c8d551e";
+const char* GIT_DATE = "Wed Apr 18 13:32:28 2018";
+const char* GIT_COMMIT = "214b76a31c71f1994a784f9b1469f96820c5b8e6";
 
 #endif /* gitVersion_hpp */
