aac87eada2b89d10e379ab40db34dc63d9f74b3f
mauraisa <aaron.maurais@bc.edu>
2018-05-23 19:57:40 -0400

    Making cpPleiades put program in local folder instead of scripts.

--- cpPleiades.sh
+++ cpPleiades.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-rsync -vr -e 'ssh -p 22022' --exclude='.*' build.sh helpFile.pdf db include src mauraisa@pleiades.bc.edu:~/scripts/DTarray_pro/
+rsync -vr -e 'ssh -p 22022' --exclude='.*' build.sh helpFile.pdf db include src mauraisa@pleiades.bc.edu:~/local/DTarray_pro/
--- include/gitVersion.hpp
+++ include/gitVersion.hpp
@@ -2,7 +2,7 @@
 #ifndef gitVersion_hpp
 #define gitVersion_hpp
 
-const char* GIT_DATE = "Wed May 23 17:26:43 2018";
-const char* GIT_COMMIT = "7e6b61d6731f57ba3620a2e574f96e89cde9ff29";
+const char* GIT_DATE = "Wed May 23 17:55:40 2018";
+const char* GIT_COMMIT = "86279e15ed14a64185cad6bd69c73d22ac57db9e";
 
 #endif /* gitVersion_hpp */
