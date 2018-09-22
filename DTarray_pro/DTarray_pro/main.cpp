f23be9a31e9d58348a008bee4c934184569130ff
Aaron Maurais <Aaron@Aarons-MacBook-Pro.local>
2018-09-22 10:49:23 -0400

    Adding a config script which actually works.

---
+++ DTarray_pro/DTarray_pro/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DTarray_pro
+//
+//  Created by Aaron Maurais on 9/22/18.
+//  Copyright (c) 2018 Aaron Maurais. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
