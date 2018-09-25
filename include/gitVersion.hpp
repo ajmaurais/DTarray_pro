ed50547d9439a8be28edfecd7b1806e019118aef
mauraisa <aaron.maurais@bc.edu>
2018-09-25 18:27:05 -0400

    Renameing installation_step_by_step to DTarray_pro-Userguide.

--- include/gitVersion.hpp
+++ include/gitVersion.hpp
@@ -2,7 +2,7 @@
 #ifndef gitVersion_hpp
 #define gitVersion_hpp
 
-const char* GIT_DATE = "Tue Sep 25 11:10:46 2018";
-const char* GIT_COMMIT = "092104c1da12d33025a506344891aed9f3a22585";
+const char* GIT_DATE = "Tue Sep 25 16:57:27 2018";
+const char* GIT_COMMIT = "c2f6d849212c8f91751fca76854fcf4520e59c18";
 
 #endif /* gitVersion_hpp */
