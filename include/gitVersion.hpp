ee0e1d9f18bfada6484acdd53863b7ad4cb2468e
mauraisa <aaron.maurais@bc.edu>
2018-09-26 17:00:05 -0400

    Adding Usage section to DTarray_pro-Userguide.

--- include/gitVersion.hpp
+++ include/gitVersion.hpp
@@ -2,7 +2,7 @@
 #ifndef gitVersion_hpp
 #define gitVersion_hpp
 
-const char* GIT_DATE = "Tue Sep 25 16:57:27 2018";
-const char* GIT_COMMIT = "c2f6d849212c8f91751fca76854fcf4520e59c18";
+const char* GIT_DATE = "Tue Sep 25 18:27:05 2018";
+const char* GIT_COMMIT = "ed50547d9439a8be28edfecd7b1806e019118aef";
 
 #endif /* gitVersion_hpp */
