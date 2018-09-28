9befbf52edb080489d705657abea793608b3b84f
mauraisa <aaron.maurais@bc.edu>
2018-09-28 19:07:59 -0400

    Adding include/gitVersion.hpp

--- include/gitVersion.hpp
+++ include/gitVersion.hpp
@@ -2,7 +2,7 @@
 #ifndef gitVersion_hpp
 #define gitVersion_hpp
 
-const char* GIT_DATE = "Fri Sep 28 18:48:17 2018";
-const char* GIT_COMMIT = "a77242142af69f3ed1927f306cfefd522ce22efa";
+const char* GIT_DATE = "Fri Sep 28 19:07:14 2018";
+const char* GIT_COMMIT = "fd323214c080b978150607e3a9133a6a4043934b";
 
 #endif /* gitVersion_hpp */
