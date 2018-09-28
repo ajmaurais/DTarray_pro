30561ba2bb2f3c65ece3814dc93112866a879012
mauraisa <aaron.maurais@bc.edu>
2018-09-28 19:27:17 -0400

    Editing DTarray_pro-Userguide.tex

--- include/gitVersion.hpp
+++ include/gitVersion.hpp
@@ -2,7 +2,7 @@
 #ifndef gitVersion_hpp
 #define gitVersion_hpp
 
-const char* GIT_DATE = "Fri Sep 28 19:07:14 2018";
-const char* GIT_COMMIT = "fd323214c080b978150607e3a9133a6a4043934b";
+const char* GIT_DATE = "Fri Sep 28 19:27:17 2018";
+const char* GIT_COMMIT = "3cacc952bdc352da3b8aab1d87882f551e8f1d81";
 
 #endif /* gitVersion_hpp */
