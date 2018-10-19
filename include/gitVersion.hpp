236ce075bbc46a4d8372e72d303a4cf5ae49e94e
mauraisa <aaron.maurais@bc.edu>
2018-10-19 11:48:05 -0400

    Fixing typos in DTarray_pro-Userguide

--- include/gitVersion.hpp
+++ include/gitVersion.hpp
@@ -2,7 +2,7 @@
 #ifndef gitVersion_hpp
 #define gitVersion_hpp
 
-const char* GIT_DATE = "Wed Oct 10 15:28:54 2018";
-const char* GIT_COMMIT = "63993b5a76fef2e3e093189db10b338546550bfe";
+const char* GIT_DATE = "Wed Oct 10 15:30:06 2018";
+const char* GIT_COMMIT = "b52f6ef4d95d7c590ee432ca197eb20cb81c5057";
 
 #endif /* gitVersion_hpp */
