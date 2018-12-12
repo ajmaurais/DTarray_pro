284e54a9e0b928c0cefb6dfd7d2185f13ffaa16a
mauraisa <aaron.maurais@bc.edu>
2018-12-11 19:00:28 -0500

    Adding doxygen documentation.

--- include/gitVersion.hpp
+++ include/gitVersion.hpp
@@ -2,7 +2,7 @@
 #ifndef gitVersion_hpp
 #define gitVersion_hpp
 
-const char* GIT_DATE = "Wed Oct 10 15:30:06 2018";
-const char* GIT_COMMIT = "b52f6ef4d95d7c590ee432ca197eb20cb81c5057";
+const char* GIT_DATE = "Wed Oct 24 20:49:08 2018";
+const char* GIT_COMMIT = "250cc1364cc6bf9ee36bddf911416e651fd3ee41";
 
 #endif /* gitVersion_hpp */
