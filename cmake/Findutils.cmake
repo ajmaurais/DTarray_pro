f976032b943ddbe5ef4f2a286e52189ce42c4fc7
mauraisa <aaron.maurais@bc.edu>
2020-03-11 12:09:07 -0400

    Add minimal CMakeList.txt.
    Still needs more polishing.

---
+++ cmake/Findutils.cmake
@@ -0,0 +1,24 @@
+
+set(FIND_UTILS_PATHS
+        ${PROJECT_SOURCE_DIR}/../utils
+        ${PROJECT_SOURCE_DIR}/../build/utils
+        ${PROJECT_SOURCE_DIR}/../utils_build
+        ${PROJECT_SOURCE_DIR}/../lib
+        /usr/local
+        /usr/local/lib)
+
+find_path(UTILS_INCLUDE_DIR
+            bufferFile.hpp
+            fastaFile.hpp
+            molecularFormula.hpp
+            peptideUtils.hpp
+            tsvFile.hpp
+            utils.hpp
+        PATH_SUFFIXES include
+        PATHS ${FIND_UTILS_PATHS})
+
+find_library(UTILS_LIB
+        NAMES utils libutils.a libutils
+        PATHS ${FIND_UTILS_PATHS}
+        DOC "Path to utils library.")
+
